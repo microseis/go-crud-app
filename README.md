bb0491864a195e59fcaa1a9ea01087282e71e3e9
Aleksei Stepanov <stepanov.geo@ya.ru>
2024-04-01 17:02:45 +0300

    add README.md & .env.example

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple Go CRUD APP
+
+Simple REST app in Golang 
+
+## Features:
+- Use of  Gin Web Framework
+- Connection to Postgres 
+- Use of GORM library
+
+## Run
+
+Configure environment variables in `.env` as provided in .env.example and then run the following command to serve the app: 
+
+```go run main.go```
--- main.go
+++ main.go
@@ -3,9 +3,24 @@ package main
 import (
 	"aleksei/go/db"
 	"aleksei/go/router"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
+	var (
+		APP_HOST = os.Getenv("APP_HOST")
+		APP_PORT = os.Getenv("APP_PORT")
+	)
+
 	db.InitPostgresDB()
-	router.InitRouter().Run("localhost:5000")
+	router.InitRouter().Run(APP_HOST + ":" + APP_PORT)
 }
